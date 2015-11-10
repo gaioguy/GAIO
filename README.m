53bb8357f9fbe279d8cc46f4f0bb7a1f09d7b62c
Oliver Junge <gaioguy@users.noreply.github.com>
2015-11-10 15:44:15 +0100

    Update README.m

--- README.m
+++ README.m
@@ -1,25 +1,10 @@
-GAIO 3.0
+# GAIO
 
-A Matlab toolbox for the global analysis of invariant objects
-in dynamical systems.
+A Matlab toolbox for the global analysis of invariant objects in dynamical systems.
 
+## Installation
 
-Installation
-------------
-
-Unpack the archive, i.e.
-
-> unzip GAIO-3.0.zip
-
-and add the resulting folder to your Matlab path, e.g.
-
->> addpath(genpath('/Users/junge/GAIO-3.0'))
-
-
-Bug reports
------------
-
-If something does not work, please send an email to oj@tum.de
-
+After unpacking, add the resulting folder to your Matlab path, e.g.
+>> addpath(genpath('/Users/junge/GAIO'))
 
 Copyright (c) 2003-2013 by djs2 GbR, Paderborn
