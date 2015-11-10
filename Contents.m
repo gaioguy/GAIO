2c235e4c56772eddee221994720a8a034f4166bb
gaioguy <oj@tum.de>
2015-11-10 17:25:38 +0100

    initial commit of Contents.m

---
+++ Contents.m
@@ -0,0 +1,20 @@
+% GAIO
+%
+% GAIO is a toolbox for the set oriented numerical analysis of
+% dynamical systems. It provides data structures and algorithms
+% for the computation of
+%   - invariant sets (maximal invariant set, chain recurrent set,
+%     periodic points, global attractor);
+%   - invariant manifolds (stable/unstable manifold of an arbitrary
+%     invariant set);
+%   - invariant measures, almost invariant sets
+%   - value functions for optimal control problems
+%
+% data structure
+%   Tree         - basic data structure for storing the set collections
+%
+% functions
+%   tpgraph      - transition probability graph
+%   dpgraph      - dynamic programming graph
+%   dijkstra     - shortest paths in a directed graph 
+%   boxplot      - box plot in 2D and 3D
