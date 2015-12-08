20cf28be386325ee9a1a0bbc04acb1421da773ba
gaioguy <oj@tum.de>
2015-12-08 11:38:12 +0100

    ""

--- demos/lorenz_fast.m
+++ demos/lorenz_fast.m
@@ -9,33 +9,34 @@ v = @(x) [s*(x(:,2)-x(:,1)) ...
 h = 0.01; n = 20; f = @(x) rk4(v,x,h,n);        % flow map
 n = 7; x = linspace(-1,1,n)'; [XX,YY,ZZ] = meshgrid(x,x,x);
 X = [ XX(:) YY(:) ZZ(:) ];                      % sample points (uniform grid)
-c = [0 0 27]; r = [30 30 40]; B = Tree(c,r);    % the box collection
+c = [0 0 27]; r = [30 30 40]; tree = Tree(c,r);    % the box collection
 
 %% continuation algorithm
-d = 3; depth = 18; 
+d = 3; depth = 21; 
 x0 = [sqrt(b*(rh-1)) sqrt(b*(rh-1)) rh-1];      % equilibrium  
-B.insert(x0', depth);
+tree.insert(x0', depth);
 
 none = 0; hit = 1; ins = 2; expd = 4; 
-n0 = 0; n1 = B.count(depth); tic
+n0 = 0; n1 = tree.count(depth); tic
 while n1 > n0                                   % while boxes are being added
-  B.change_flags('all', ins, expd);             % flag inserted boxes for expansion
-  b = B.boxes(depth);                           
+  tree.change_flags('all', ins, expd);             % flag inserted boxes for expansion
+  b = tree.boxes(depth);                           
   flags = b(2*d+1,:); 
   I = find(bitand(flags,expd));                 % find boxes to be expanded  
   c = b(1:d,I); r = b(d+1:2*d,1);               % get center and radii
   n = length(I); E = ones(n,1);
   P = kron(E,X)*diag(r) + kron(c',ones(size(X,1),1)); % sample points
-  B.insert(f(P)', depth, ins, none);            % insert boxes which are hit by f(P)
-  B.unset_flags('all', expd);                   % unflag recently expanded boxes
-  n0 = n1; n1 = B.count(depth); 
+  tree.insert(f(P)', depth, ins, none);            % insert boxes which are hit by f(P)
+  tree.unset_flags('all', expd);                   % unflag recently expanded boxes
+  n0 = n1; n1 = tree.count(depth); 
   disp(sprintf('%d boxes', n1));
+  boxplot3(tree); view(20,30); axis square; axis tight; drawnow; pause
 end
 toc
 
 %% plot
-boxplot3(B); view(20,30); axis square; axis tight;
+boxplot3(tree); view(20,30); axis square; axis tight;
 xlabel('x'); ylabel('y'); zlabel('z'); 
 
 %% cleanup
-delete(B);
+delete(tree);
