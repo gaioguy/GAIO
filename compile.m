0bee853fb21ecddf81979c11a5c18e7992fe2f03
Oliver Junge <junge@lapjunge2.ma.tum.de>
2017-06-06 13:42:25 +0200

    various fixes

--- compile.m
+++ compile.m
@@ -6,12 +6,12 @@
 % Note: Your Matlab mex compiler needs to be be setup first ('mex -setup')
 
 cd matlab
-mex -largeArrayDims -D_MEASURE_ -I../lib tree_.c "../lib/*.c"
-mex -largeArrayDims -D_MEASURE_ -I../lib dijkstra.c "../lib/*.c"
-mex -largeArrayDims -D_MEASURE_ -I../lib scc.c "../lib/*.c"
-mex -largeArrayDims -D_MEASURE_ -I../lib subshift.c "../lib/*.c"
-mex -largeArrayDims -D_MEASURE_ -I../lib fwd_inv_set.c "../lib/*.c"
-cd ..; cd @Tree
+mex -largeArrayDims -D_MEASURE_ -I../lib tree_.c ../lib/*.c
+mex -largeArrayDims -D_MEASURE_ -I../lib subshift.c ../lib/*.c
+mex -largeArrayDims -D_MEASURE_ -I../lib dijkstra.c 
+mex -largeArrayDims -D_MEASURE_ -I../lib scc.c
+mex -largeArrayDims -D_MEASURE_ -I../lib fwd_inv_set.c
+cd ../@Tree
 mex -D_MEASURE_ -I../lib subsref.c "../lib/*.c"
 mex -D_MEASURE_ -I../lib subsasgn.c "../lib/*.c"
 mex -D_MEASURE_ -I../lib delete.c "../lib/*.c"
--- matlab/plotshadows.m
+++ matlab/plotshadows.m
@@ -14,7 +14,7 @@ yB = kron([Q(:,2)-Q(:,2+d) Q(:,2)-Q(:,2+d) Q(:,2)-Q(:,2+d) Q(:,2)-Q(:,2+d)]*0.95
 zB = [B(:,3)-B(:,3+d) B(:,3)-B(:,3+d) B(:,3)+B(:,3+d) B(:,3)+B(:,3+d)];
 if (az > 90) | (az < -90), 
     p = patch(xQ',yQ',zQ',[0.8 0.8 0.8],'edgealpha',0); 
-    patch(xB',yB',zB','k','edgealpha',0,'facealpha',0.2); 
+    patch(xB',yB',zB','k','edgealpha',0,'facealpha',0.5); 
 end
 % front
 xQ = [Q(:,1)-Q(:,1+d) Q(:,1)+Q(:,1+d) Q(:,1)+Q(:,1+d) Q(:,1)-Q(:,1+d)];
@@ -25,7 +25,7 @@ yB = kron([Q(:,2)+Q(:,2+d) Q(:,2)+Q(:,2+d) Q(:,2)+Q(:,2+d) Q(:,2)+Q(:,2+d)]*0.95
 zB = [B(:,3)-B(:,3+d) B(:,3)-B(:,3+d) B(:,3)+B(:,3+d) B(:,3)+B(:,3+d)];
 if az > -90 & az < 90, 
     patch(xQ',yQ',zQ',[0.8 0.8 0.8],'edgealpha',0); 
-    patch(xB',yB',zB','k','edgealpha',0,'facealpha',0.2); 
+    patch(xB',yB',zB','k','edgealpha',0,'facealpha',0.5); 
 end
 % right
 xQ = [Q(:,1)+Q(:,1+d) Q(:,1)+Q(:,1+d) Q(:,1)+Q(:,1+d) Q(:,1)+Q(:,1+d)];
@@ -36,7 +36,7 @@ zB = [B(:,3)-B(:,3+d) B(:,3)-B(:,3+d) B(:,3)+B(:,3+d) B(:,3)+B(:,3+d)];
 xB = kron([Q(:,1)+Q(:,1+d) Q(:,1)+Q(:,1+d) Q(:,1)+Q(:,1+d) Q(:,1)+Q(:,1+d)]*0.95,  ones(size(xB,1),1));
 if az < 0, 
     patch(xQ',yQ',zQ',[0.8 0.8 0.8],'edgealpha',0); 
-    patch(xB',yB',zB','k','edgealpha',0,'facealpha',0.2); 
+    patch(xB',yB',zB','k','edgealpha',0,'facealpha',0.5); 
 end
 % left
 xQ = [Q(:,1)-Q(:,1+d) Q(:,1)-Q(:,1+d) Q(:,1)-Q(:,1+d) Q(:,1)-Q(:,1+d)];
@@ -47,7 +47,7 @@ zB = [B(:,3)-B(:,3+d) B(:,3)-B(:,3+d) B(:,3)+B(:,3+d) B(:,3)+B(:,3+d)];
 xB = kron([Q(:,1)-Q(:,1+d) Q(:,1)-Q(:,1+d) Q(:,1)-Q(:,1+d) Q(:,1)-Q(:,1+d)]*0.95,  ones(size(xB,1),1));
 if az > 0, 
     patch(xQ',yQ',zQ',[0.8 0.8 0.8],'edgealpha',0); 
-    patch(xB',yB',zB','k','edgealpha',0,'facealpha',0.2); 
+    patch(xB',yB',zB','k','edgealpha',0,'facealpha',0.5); 
 end
 % top
 xQ = [Q(:,1)-Q(:,1+d) Q(:,1)+Q(:,1+d) Q(:,1)+Q(:,1+d) Q(:,1)-Q(:,1+d)];
@@ -58,7 +58,7 @@ yB = [B(:,2)-B(:,2+d) B(:,2)-B(:,2+d) B(:,2)+B(:,2+d) B(:,2)+B(:,2+d)];
 zB = kron([Q(:,3)+Q(:,3+d) Q(:,3)+Q(:,3+d) Q(:,3)+Q(:,3+d) Q(:,3)+Q(:,3+d)]*0.95, ones(size(xB,1),1));
 if el < 0, 
     patch(xQ',yQ',zQ',[0.8 0.8 0.8],'edgealpha',0); 
-    patch(xB',yB',zB','k','edgealpha',0,'facealpha',0.2); 
+    patch(xB',yB',zB','k','edgealpha',0,'facealpha',0.5); 
 end
 % bottom
 xQ = [Q(:,1)-Q(:,1+d) Q(:,1)+Q(:,1+d) Q(:,1)+Q(:,1+d) Q(:,1)-Q(:,1+d)];
@@ -69,7 +69,7 @@ yB = [B(:,2)-B(:,2+d) B(:,2)-B(:,2+d) B(:,2)+B(:,2+d) B(:,2)+B(:,2+d)];
 zB = kron([Q(:,3)-Q(:,3+d) Q(:,3)-Q(:,3+d) Q(:,3)-Q(:,3+d) Q(:,3)-Q(:,3+d)]*0.95, ones(size(xB,1),1));
 if el > 0, 
     patch(xQ',yQ',zQ',[0.8 0.8 0.8],'edgealpha',0); 
-    patch(xB',yB',zB','k','edgealpha',0,'facealpha',0.2); 
+    patch(xB',yB',zB','k','edgealpha',0,'facealpha',0.5); 
 end
 
 
