40b01a61cbdb7114812ddd1d4fa5141e174eed79
Oliver Junge <oj@tum.de>
2021-03-22 12:01:01 +0100

    simplification of chain_recurrent.m

--- demos/chain_recurrent.m
+++ demos/chain_recurrent.m
@@ -1,31 +1,21 @@
-% GAIO demo
-% chain recurrent set for the Henon map
+% GAIO demo: chain recurrent set in the Henon map
 
-% Henon map
-a = 1.3; b = 0.2;
-f = @(x) [1-a*x(:,1).^2+x(:,2)./5 b*x(:,1)*5];   
-% sample points on the edges of the unit square
-n = 40; X1 = linspace(-1,1,n)'; 
-X = [ X1 -ones(size(X1)); X1 ones(size(X1)); -ones(size(X1)) X1; ones(size(X1)) X1];    
-% the tree
-c = [0 0]; r = [1.5 1.5]; t = Tree(c, r);
+% the Henon map
+a = 1.4; b = 0.3;
+f = @(x) [1-a*x(:,1).^2+x(:,2) b*x(:,1)];   
 
-%% subdivision algorithm
-dim = t.dim; hit = 1; sd = 8; depth = 14; tic
-for d = 1:depth
-  t.set_flags('all', sd);
-  t.subdivide;
-  G = tpmatrix(t, f, X, d, 0);
-  sc = scc(G,G');
-  sc(find(sc > -1)) = 1; sc(find(sc == -1)) = 0;
-  flags = sprintf('%1d', sc);
-  t.set_flags(flags, hit);
-  t.remove(hit);
-  fprintf('step %d: %d boxes\r', d, t.count(-1));
-end
+% choose sample points in reference square
+n = 10; X1 = linspace(-1,1,n); 
+[XX,YY] = meshgrid(X1,X1);
+X = [XX(:) YY(:)];
 
-%% plot
-boxplot2(t); xlabel('$x$'); ylabel('$y$');
+% initialize the GAIO tree which stores the box covering
+center = [0 0]; radius = [1.5 1.5]; 
+tree = Tree(center, radius);
 
-%% clean up
-delete(t);
+% compute the chain recurrent set
+depth = 16;
+crs(tree, f, X, depth);
+
+% plot of the box covering
+boxplot2(tree,'edgecolor','r'); 
